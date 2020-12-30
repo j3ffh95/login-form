a6a3424cff452aaf8ec52cb79d3d83ac9a3716f0
j3ffh95 <j3ffh95@gmail.com>
2020-12-29 22:05:20 -0500

    Created login button variable

---
+++ main.js
@@ -0,0 +1,14 @@
+// onclick = "document.getElementById('id01').style.display='block'";
+// style = "width: auto";
+
+const loginButton = document.getElementById("login-btn");
+
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
