273c72de731121d193ce24097c4cb22052ff3c83
j3ffh95 <j3ffh95@gmail.com>
2020-12-30 23:45:27 -0500

    modal variable

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
+// Get the modal
+const modal = document.getElementById("modal-container");
+
 const loginButton = document.getElementById("login-btn");
 // loginButton.style.width = "auto";
 
 loginButton.addEventListener("click", function (e) {
-  const modal = document.getElementById("modal-container");
-
   modal.style.display = "block";
 });
-
-// Get the modal
-var modal = document.getElementById("modal-container");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
