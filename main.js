72d1772ee6027d569ee0587436b0fbfb4cc4eec9
j3ffh95 <j3ffh95@gmail.com>
2020-12-30 21:57:06 -0500

    Fixing the container content inside the form

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const loginButton = document.getElementById("login-btn");
 // loginButton.style.width = "auto";
 
 loginButton.addEventListener("click", function (e) {
-  const modal = document.getElementById("id01");
+  const modal = document.getElementById("modal-container");
 
   modal.style.display = "block";
 });
 
 // Get the modal
-var modal = document.getElementById("id01");
+var modal = document.getElementById("modal-container");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
